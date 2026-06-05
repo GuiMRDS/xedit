f9884004bbb8cd9d69554c2308f823e6670ff244
Guilherme Marinho Santos <131541140+GuiMRDS@users.noreply.github.com>
2026-06-05 19:44:07 +0000

    feat: add abstrution method POST in `\api\v1\status`

---
+++ infra/errors.js
@@ -0,0 +1,38 @@
+export class InternalServerError extends Error {
+  constructor({ cause }) {
+    super("Um error interno não esperando aconteceu.", {
+      cause,
+    });
+    this.name = "InternalServerError";
+    this.action = "Entre em contato com o suporte";
+    this.statusCode = 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      statusCode: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Metodo não permitido para este endpoint.");
+    this.name = "MethodNotAllowedError";
+    this.action =
+      "Verifique se o método HTTP enviado é válido para este endpoint.";
+    this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      statusCode: this.statusCode,
+    };
+  }
+}
