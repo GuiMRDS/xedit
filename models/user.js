c73fc78befd86338879a55663f10a1161e97d7fb
Guilherme Marinho Santos <131541140+GuiMRDS@users.noreply.github.com>
2026-06-09 21:11:17 +0000

    feat: create method `ValidationError`

--- models/user.js
+++ models/user.js
@@ -1,8 +1,36 @@
 import database from "infra/database";
+import { ValidationError } from "infra/errors";
 
 async function create(userInputValue) {
-  const result = await database.query({
-    text: `
+  await validateUniqueEmail(userInputValue.email);
+
+  const newUser = await runInsertQuery(userInputValue);
+  return newUser;
+
+  async function validateUniqueEmail(email) {
+    const result = await database.query({
+      text: `
+    SELECT  
+      email
+    FROM
+      users
+    WHERE
+      LOWER(email) = LOWER($1)
+    ;`,
+      values: [email],
+    });
+
+    if (result.rowCount > 0) {
+      throw new ValidationError({
+        message: "O email informando já estpa sendo utilizando.",
+        action: "Ultilize outro email para realizar o cadastro do usuario.",
+      });
+    }
+  }
+
+  async function runInsertQuery() {
+    const result = await database.query({
+      text: `
     INSERT INTO 
       users (username, email, password) 
     VALUES 
@@ -10,13 +38,14 @@ async function create(userInputValue) {
     RETURNING
       *
     ;`,
-    values: [
-      userInputValue.username,
-      userInputValue.email,
-      userInputValue.password,
-    ],
-  });
-  return result.rows[0];
+      values: [
+        userInputValue.username,
+        userInputValue.email,
+        userInputValue.password,
+      ],
+    });
+    return result.rows[0];
+  }
 }
 
 const user = { create };
