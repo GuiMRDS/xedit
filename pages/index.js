fb677de64e10e3d31f254e9e13dffe0891e33709
Guilherme Marinho Santos <131541140+GuiMRDS@users.noreply.github.com>
2026-06-10 16:26:15 +0000

    style: remove test in home page

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,6 @@ function Home() {
   return (
     <>
       <h1>Hello World</h1>
-
-      <h3>Quer namorar comigo?</h3>
-
-      <p>
-        Não deu certo, ela não aceitou kkkkk Mas pelo menos aprendi mais sobre o
-        Vercel e Deploy kkkkkk
-      </p>
     </>
   );
 }
