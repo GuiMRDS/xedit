b9facaaaa83e6984da48961a6d54a753a34b3cd2
Guilherme Marinho Santos <131541140+GuiMRDS@users.noreply.github.com>
2026-07-03 21:29:15 +0000

    feat: require `create:user` to access `api\v1\users`

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -5,7 +5,8 @@ import activation from "models/activation";
 
 const router = createRouter();
 
-router.post(postHandler);
+router.use(controller.injectAnonymousOrUser);
+router.post(controller.canRequest("create:user"), postHandler);
 
 export default router.handler(controller.errorHandlers);
 
@@ -13,9 +14,7 @@ async function postHandler(request, response) {
   const userInputValue = request.body;
   const newUser = await user.create(userInputValue);
 
-  // 1. Criar o Token de ativação
   const activationToken = await activation.create(newUser.id);
-  // 2. Enviar esse Token por Email
   await activation.sendEmailToUser(newUser, activationToken);
 
   return response.status(201).json(newUser);
